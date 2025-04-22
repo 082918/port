6bec04cafcf0049b1d31f7cd5fa9a2d93d6aa499
가을 <ssruyj510@gmail.com>
2025-04-22 14:36:46 +0900

    페이지 수정

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,12 +5,19 @@ import "../style/Profile.css";
 function Profile() {
   return (
     <>
-      <p>Profile</p>
-      <div>
-        <h1> ㅇㄹㄴ </h1>
-        <h2> ㄴ12342 </h2>
-        <p> 23123 </p>
+      <p className="title">Profile</p>
+      <div className="profileContainer">
+        <div className="profileBox">
+          <h2> 안녕하세요. 저는 개발자 </h2>
+          <div>
+            <h1 className="nameBox"> 최윤진 </h1>
+            <h1 className="nameBox"> 입니다. </h1>
+          </div>
+          <p> 사용자가 제일 먼저 접하는 프론트엔드. </p>
+        </div>
+        <div className="photoBox"> </div>
       </div>
+      <div> 테스트 박스 </div>
     </>
   );
 }
